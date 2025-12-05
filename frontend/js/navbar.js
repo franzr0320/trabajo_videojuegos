412e687e6646c235b65f75a4ca12e61caa7943b5
Franz0320 <frosas@fi.uba.ar>
2025-12-05 19:36:59 -0300

    Añado función que el botón iniciar sesion, cargue la página como sesion iniciado y el botón cerrar sesión, cierre sesión

--- frontend/js/navbar.js
+++ frontend/js/navbar.js
@@ -1,4 +1,4 @@
-let isLoggedIn = true;
+let isLoggedIn = false;
 
 document.getElementById("navbar-container").innerHTML = `
 <nav class="navbar" role="navigation" aria-label="main navigation">
@@ -45,9 +45,23 @@ function renderNavbarAuth() {
   }
 }
 
+// Hace que el botón cerrar sesión isLoggedIn = false y el botón iniciar sesión isLoggedIn = True //
+document.addEventListener("click", function (event) {
+  if (event.target && event.target.id === "cerrar-sesion") {
+    event.preventDefault();
+    isLoggedIn = false;
+    renderNavbarAuth();
+  }
+  if (event.target && event.target.id === "boton-login-modal") {
+    event.preventDefault();
+    isLoggedIn = true;
+    renderNavbarAuth();
+  }
+});
+
 renderNavbarAuth();
 
- // Bloquea botones que requieren el Login //
+// Bloquea botones que requieren el Login //
 document.addEventListener("click", function (event) {
   if (event.target.classList.contains("requiere-login") && !isLoggedIn) {
     event.preventDefault(); 
