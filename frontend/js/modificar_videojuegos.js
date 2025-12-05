7792aefa90c0334814b8b6967a6f3067bf41a7fd
Franz0320 <frosas@fi.uba.ar>
2025-12-05 18:40:35 -0300

    Quito funcionalidad de que aparezacn los botones (causaba dificultad al quere abrir modales), ahora los botones aparecen  cada que entremos a 'MIS VIDEOJEUGOS' y añado archivo modificar_videojuego.js de donde se despliega el modal parta modificar el videojuego

---
+++ frontend/js/modificar_videojuegos.js
@@ -0,0 +1,63 @@
+document.getElementById("modificar_videojuego-container").innerHTML = `
+<div class="modal" id="modal-modificar">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">EDITAR VIDEOJUEGO</p>
+      <button class="delete" aria-label="close"></button>
+    </header>
+    <section class="modal-card-body">
+      <!-- Content ... -->
+      <div class="field">
+        <label class="label"> Título del Videojuego</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="The Last of Us">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Año de lanzamiento</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="2013">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Breve descripción</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="125 caracteres MAX">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Imagen Portada Link </label>
+        <div class="control">
+          <input class="input" type="text" placeholder="https://example.com/imagen.jpg">
+        </div>
+      </div>
+
+      <div class="field is-grouped">
+        <div class="control">
+          <button class="button is-link requiere-login">Guardar</button>
+        </div>
+        <div class="control">
+          <button class="button is-link is-light">Cancelar</button>
+        </div>
+      </div>
+
+    </section>
+  </div>
+</div>
+`;
+// MODAL AÑADIR VIDEOJUEGO //
+const modalModificar = document.getElementById('modal-modificar');
+const botonesAbrir = document.querySelectorAll('.abrir-modal-modificar');
+const botonCerrar = modalModificar.querySelector('.delete');
+const fondoModal = modalModificar.querySelector('.modal-background');
+
+botonesAbrir.forEach(boton => {
+boton.onclick = () => modalModificar.classList.add('is-active');
+});
+
+botonCerrar.onclick = () => modalModificar.classList.remove('is-active');
+fondoModal.onclick = () => modalModificar.classList.remove('is-active');
