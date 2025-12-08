9b6e07f50deff3b1ea1b8695c2bbde1960d9c490
Franz0320 <frosas@fi.uba.ar>
2025-12-08 17:43:01 -0300

    Implemento CRUD completo de videojuegos de usuario y corrijo validaciones en API

--- backend/src/api.js
+++ backend/src/api.js
@@ -12,14 +12,19 @@ const {
   getOneVideojuegoBase,
   createVideojuegoBase,
   deleteVideojuegoBase,
-  updateVideojuegoBase
+  updateVideojuegoBase,
+  getAllVideojuegosUsuario,
+  getOneVideojuegoUsuario,
+  createVideojuegoUsuario,
+  deleteVideojuegoUsuario,
+  updateVideojuegoUsuario
 } = require('./db/db')
 
 app.get('/api/health', (req, res) => {
   res.json({ message: 'Hello from the API'});
 });
 
-//VIDEOJUEGOS
+//VIDEOJUEGOS_BASE
 //get all videojuegos
 app.get('/api/videojuegos', async (req, res) => {
   const videojuegos = await getAllVideojuegosBase();
@@ -156,6 +161,128 @@ app.put('/api/videojuegos/:id', async (req, res) => {
 
   res.json({ status: 'OK', videojuego: videojuego });
 });
+
+// VIDEOJUEGOS_USUARIO 
+// get all videojeugo_usuario
+app.get('/api/videojuegos-usuario/:usuario_id', async (req, res) => {
+  const videojuegos = await getAllVideojuegosUsuario(req.params.usuario_id);
+  res.json(videojuegos);
+});
+
+// get one videojeugo_usuario
+app.get('/api/videojuegos-usuario/:usuario_id/:id', async (req, res) => {
+  const videojuego = await getOneVideojuegoUsuario(req.params.id, req.params.usuario_id);
+
+  if (!videojuego) {
+    return res.status(404).json({ error: "Videojuego no encontrado" });
+  }
+  res.json(videojuego);
+});
+
+// create videojeugo_usuario
+app.post('/api/videojuegos-usuario', async (req, res) => {
+
+  if (!req.body.usuario_id ||
+      !req.body.titulo ||
+      !req.body.genero ||
+      !req.body.anio ||
+      !req.body.historia_principal ||
+      !req.body.descripcion ||
+      !req.body.portada) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+
+  if (typeof req.body.genero !== "string" || req.body.genero.trim().length === 0) {
+    return res.status(400).json({ error: "El género debe ser un texto válido" });
+  }
+
+  if (isNaN(req.body.anio)) {
+    return res.status(400).json({ error: "El año debe ser un número" });
+  }
+
+  if (isNaN(req.body.historia_principal) || req.body.historia_principal < 0) {
+    return res.status(400).json({ error: "La historia principal debe ser un número mayor o igual a 0" });
+  }
+
+  if (req.body.descripcion.length > 108) {
+    return res.status(400).json({ error: "La descripción no puede superar los 107 caracteres" });
+  }
+
+  const videojuego = await createVideojuegoUsuario(
+    req.body.usuario_id,
+    req.body.titulo,
+    req.body.genero,
+    req.body.anio,
+    req.body.historia_principal,
+    req.body.descripcion,
+    req.body.portada
+  );
+
+  res.json({ status: "OK", videojuego: videojuego });
+});
+
+// update videojuego_usuario
+app.put('/api/videojuegos-usuario/:usuario_id/:id', async (req, res) => {
+
+  if (!req.body.titulo ||
+      !req.body.genero ||
+      !req.body.anio ||
+      !req.body.historia_principal ||
+      !req.body.descripcion ||
+      !req.body.portada) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+
+  if (typeof req.body.genero !== "string" || req.body.genero.trim().length === 0) {
+    return res.status(400).json({ error: "El género debe ser un texto válido" });
+  }
+
+  if (isNaN(req.body.anio)) {
+    return res.status(400).json({ error: "El año debe ser un número" });
+  }
+
+  if (isNaN(req.body.historia_principal) || req.body.historia_principal < 0) {
+    return res.status(400).json({ error: "La historia principal debe ser un número mayor o igual a 0" });
+  }
+
+  if (req.body.descripcion.length > 108) {
+    return res.status(400).json({ error: "La descripción no puede superar los 107 caracteres" });
+  }
+
+  const videojuego = await updateVideojuegoUsuario(
+    req.params.id,
+    req.params.usuario_id,
+    req.body.titulo,
+    req.body.genero,
+    req.body.anio,
+    req.body.historia_principal,
+    req.body.descripcion,
+    req.body.portada
+  );
+
+  if (!videojuego) {
+    return res.status(404).json({ error: "Videojuego no encontrado" });
+  }
+
+  res.json({ status: "OK", videojuego: videojuego });
+});
+
+// delete videojuego_usuario
+app.delete('/api/videojuegos-usuario/:usuario_id/:id', async (req, res) => {
+  const videojuego = await deleteVideojuegoUsuario(req.params.id, req.params.usuario_id);
+
+  if (!videojuego) {
+    return res.status(404).json({ error: "Videojuego no encontrado" });
+  }
+
+  res.json({ status: "OK", id: videojuego });
+});
+
+
+
 app.listen(PORT, () => {
   console.log("Server Listening on PORT:", PORT);
 });
+
+
+
